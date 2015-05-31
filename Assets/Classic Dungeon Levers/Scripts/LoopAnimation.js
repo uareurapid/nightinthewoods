3e70aa4a67da26918622475e30bf058f16c4adfe
uareurapid <cristo.paulo@gmail.com>
2015-05-31 22:15:36 +0100

    add assets/xxx

---
+++ Assets/Classic Dungeon Levers/Scripts/LoopAnimation.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+var anim1 : String;
+var anim2 : String;
+var curAnim : int = 1;
+
+function Start () {
+	animation.Play(anim1);
+	animation.wrapMode = WrapMode.Once;
+}
+function Update() {
+	if(curAnim == 1) {
+		if(!animation.isPlaying) {
+			animation.CrossFade(anim2);
+			animation.wrapMode = WrapMode.Once;
+			curAnim = 2;
+		}
+	}
+	else {
+		if(curAnim == 2) {
+			if(!animation.isPlaying) {
+				animation.CrossFade(anim1);
+				animation.wrapMode = WrapMode.Once;
+				curAnim = 1;
+			}
+		}
+	}
+}
